fc870fd7d2dc30630936d0b3edd45f4e476a4856
Mark Agne <magnenator@gmail.com>
2020-02-19 16:22:02 -0600

    added SpriteSheetData class to store parsed JSON data

---
+++ Editor/SpriteSheetData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Miscreant.Aseprite.Editor
+{
+	[Serializable]
+	public sealed class SpriteSheetData
+	{
+		[Serializable]
+		public struct Frame
+		{
+			public string filename;
+			public Rect frame;
+			public bool rotated;
+			public bool trimmed;
+			public Rect spriteSourceSize;
+			public Rect sourceSize;
+			public int duration;
+		}
+
+		[Serializable]
+		public struct Rect
+		{
+			public int x;
+			public int y;
+			public int w;
+			public int h;
+		}
+
+		[Serializable]
+		public struct MetaData
+		{
+			public string app;
+			public string version;
+			public string image;
+			public string format;
+			public Rect size;
+			public string scale;
+		}
+
+		public Frame[] frames;
+		public MetaData meta;
+	}
+}
