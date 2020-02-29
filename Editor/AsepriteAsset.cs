d5135ed1d64155054413b40ac92f16b1602b8927
Mark Agne <magnenator@gmail.com>
2020-02-29 00:15:04 -0600

    summary explanation for the AsepriteAsset ScriptableObject

--- Editor/AsepriteAsset.cs
+++ Editor/AsepriteAsset.cs
@@ -2,6 +2,9 @@
 
 namespace Miscreant.Aseprite.Editor
 {
+	/// <summary>
+	/// Dummy "main" sub-object for imported Aseprite assets. Also serves as an anchor for the thumbnail image. 
+	/// </summary>
 	public sealed class AsepriteAsset : ScriptableObject
 	{ }
 }
